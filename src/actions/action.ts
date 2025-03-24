d734ca13119b3832d9c6671c3358858c08750534
Andreo Friendly <102529981+andreofriendly@users.noreply.github.com>
2025-03-24 22:50:02 +0700

    action.ts

---
+++ src/actions/action.ts
@@ -0,0 +1,92 @@
+"use sever"
+
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "@/db/index"
+import { expense, expenseCategories, income, incomeCategories, users } from "@/db/schema";
+import { and, eq } from "drizzle-orm";
+
+//Helper current User
+const getCurrentUserId = async () => {
+    const user = await currentUser();
+    if (!user) throw new Error("Unauthorized");
+
+    await db.insert(users).values({ id: user.id }).onConflictDoNothing();
+
+    return user.id;
+};
+
+//add Income Category
+export const addIncomeCategory = async (name: string) => {
+    const userId = await getCurrentUserId();
+    await db.insert(incomeCategories).values({name,userId});
+};
+
+//add Expenses Category
+export const addExpenseCategory = async (name: string) => {
+    const userId = await getCurrentUserId();
+    await db.insert(expenseCategories).values({name,userId});
+};
+
+//add Income
+export const addIncome = async (amount: number, categoryId: number) => {
+    const userId = await getCurrentUserId();
+    await db.insert(income).values({amount, categoryId, userId});
+};
+
+//add Expense
+export const addExpense = async (amount: number, categoryId: number) => {
+    const userId = await getCurrentUserId();
+    await db.insert(expense).values({amount, categoryId, userId});
+};
+
+//get All Expenses Categories
+export const getExpenseCategories = async () => {
+    const userId = await getCurrentUserId();
+    return await db.select()
+    .from (expenseCategories)
+    .where(eq(expenseCategories.userId,userId));
+}; 
+
+//get All Income Categories
+export const getIncomeCategories = async () => {
+    const userId = await getCurrentUserId();
+    return await db.select()
+    .from (incomeCategories)
+    .where(eq(incomeCategories.userId,userId));
+}; 
+
+//get all expenses records with the category names
+export const getExpenses = async () => {
+    const userId = await getCurrentUserId();
+    return await db
+    .select({
+        id:expenseCategories.id,
+        amount:expense.amount,
+        category:expenseCategories.name,
+        createdAt:expense.createdAt,
+    })
+    .from(expense)
+    .innerJoin(expenseCategories, eq (expense.categoryId, expenseCategories.id))
+    .where(and(
+        eq(expense.userId, userId),
+        eq(expenseCategories.userId, userId)
+    ));
+}
+
+//get all income records with the category names
+export const getIncome = async () => {
+    const userId = await getCurrentUserId();
+    return await db
+    .select({
+        id:incomeCategories.id,
+        amount:income.amount,
+        category:incomeCategories.name,
+        createdAt:income.createdAt,
+    })
+    .from(income)
+    .innerJoin(incomeCategories, eq (income.categoryId, incomeCategories.id))
+    .where(and(
+        eq(income.userId, userId),
+        eq(incomeCategories.userId, userId)
+    ));
+}
--- src/db/index.ts
+++ src/db/index.ts
@@ -3,4 +3,4 @@ import { config } from 'dotenv';
 
 config({path:".env"})
 
-const db = drizzle(process.env.DATABASE_URL!);
+export const db = drizzle(process.env.DATABASE_URL!);
