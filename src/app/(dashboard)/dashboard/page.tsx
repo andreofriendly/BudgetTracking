a2e72e4dbb0873521861dd3d125a7933629801bc
Andreo Friendly <102529981+andreofriendly@users.noreply.github.com>
2025-03-25 01:44:22 +0700

    dashboard income expenses netprofit

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,42 @@
+"use client"
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { getExpenses, getIncome } from "@/actions/action";
 import ChartDashboard from "@/components/chart-dashboard";
+import { useEffect, useState } from "react";
 
 export default function Page(){
+  const [incomeData, setIncomeData] = useState<any[]>([])
+  const [expenseData, setExpenseData] = useState<any[]>([])
+
+  const totalIncome = incomeData.reduce((sum, item) => sum + item.amount ,0 );
+  const totalExpense = expenseData.reduce((sum, item) => sum + item.amount ,0 );
+  const netProfit = totalIncome - totalExpense;
+
+    useEffect(() => {
+        const loadData = async () => {
+            try{
+                const [incomeRecords, expenseRecords] = await Promise.all([
+                    getIncome(),
+                    getExpenses(),
+                ]);
+
+                setIncomeData(incomeRecords);
+                setExpenseData(expenseRecords);
+            }catch(error){
+                console.log("Failed to load categories")
+            }
+        }
+        loadData();
+    }, []);
+
+    function formatCurrency(amount: number){
+        return new Intl.NumberFormat("id-ID",{
+          style: "currency",
+          currency: "idr",
+          minimumFractionDigits: 0
+        }).format(amount)
+      }
+    
     return<>
     <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         
@@ -20,7 +56,7 @@ export default function Page(){
             <div className="rounded-xl h-36 flex items-center justify-between p-8 border-b shadow-sm">
                 <div className="flex flex-col">
                     <h2 className="text-lg font-semibold text-gray-700">Total Income</h2>
-                    <p className="text-3xl font-bold text-blue-600">Rp. 2.000.000</p>
+                    <p className="text-3xl font-bold text-blue-600">{formatCurrency(totalIncome)}</p>
                 </div>
             </div>
 
@@ -28,15 +64,15 @@ export default function Page(){
             <div className="rounded-xl h-36 flex items-center justify-between p-8 border-b shadow-sm">
                 <div className="flex flex-col">
                     <h2 className="text-lg font-semibold text-gray-700">Total Expenses</h2>
-                    <p className="text-3xl font-bold text-blue-600">Rp. 2.000.000</p>
+                    <p className="text-3xl font-bold text-blue-600">{formatCurrency(totalExpense)}</p>
                 </div>
             </div>
 
             {/* Net */}
             <div className="rounded-xl h-36 flex items-center justify-between p-8 border-b shadow-sm">
                 <div className="flex flex-col">
-                    <h2 className="text-lg font-semibold text-gray-700">Total Net</h2>
-                    <p className="text-3xl font-bold text-blue-600">Rp. 2.000.000</p>
+                    <h2 className="text-lg font-semibold text-gray-700">Net Profit</h2>
+                    <p className="text-3xl font-bold text-blue-600">{formatCurrency(netProfit)}</p>
                 </div>
             </div>
         </div>
